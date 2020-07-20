d3a6945757969c5bc95220f73353f26c368e28d4
Vishwanath Sangale <vishsangale@gmail.com>
2020-07-20 09:10:50 -0700

    Basic variables in go

---
+++ basic_vars.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+    var age int = 40
+
+    var pi float64 = 3.1415
+
+    num := 1
+
+    var name string = "vish"
+
+
+    fmt.Println(age, pi, num)
+
+    fmt.Println(name)
+
+
+    var is_success bool = true
+
+
+    fmt.Printf("%", is_success)
+
+    fmt.Printf("%T \n", is_success)
+}
